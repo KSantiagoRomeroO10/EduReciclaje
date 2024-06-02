119be36c1980babdbfe490feaed5fb7492ddca4b
Santiago Romero <KSantiagoRomeroO10@outlook.com>
2024-06-01 21:54:20 -0500

    Agregando una estructura para iconos svg y agregando el search en el header

--- frontend/src/Components/Common/Header/Header.jsx
+++ frontend/src/Components/Common/Header/Header.jsx
@@ -2,18 +2,16 @@ import style from './Header.module.css'
 
 import ButtonRoute from '../ButtonRoute/ButtonRoute'
 import ButtonMenu from '../ButtonMenu/ButtonMenu'
+import Search from './Search/Search'
 
 const Header = () => {
   return(
     <header className={style.Header}>
 
-    <ButtonRoute NombreButton={'Iniciar Sesión'} Route={'/Login'} Right={true}/>
-    <ButtonRoute NombreButton={'Registrarse'} Route={'/SingUp'} Right={true}/>
-
       <div className={style.ContainerMenu}>
-        <p className={style.Trigger}>Menu</p>
+        <p className={style.Trigger}>Explorar</p>
         <ul className={style.Menu}>
-          <p className={style.Trigger}>Menu</p>
+          <p className={style.Trigger}>Explorar</p>
           <div className={style.Border}>
             <li> <ButtonMenu NombreButton={'Foros'} Route={'/Forums'}/> </li>
             <li> <ButtonMenu NombreButton={'Quizzes'} Route={'/Quizzes'}/> </li>
@@ -22,6 +20,15 @@ const Header = () => {
             <li> <ButtonMenu NombreButton={'Comentarios y opiniones'} Route={'/Coments&Opinions'}/> </li>
           </div>
         </ul>      
+      </div>
+
+      <div className={style.Centre}>
+        <Search/>
+      </div>    
+      
+      <div className={style.Right}>
+        <ButtonRoute NombreButton={'Iniciar Sesión'} Route={'/Login'} Right={true}/>
+        <ButtonRoute NombreButton={'Registrarse'} Route={'/SingUp'} Right={true}/>  
       </div>
 
     </header>
--- frontend/src/Components/Common/Header/Search/Search.jsx
+++ frontend/src/Components/Common/Header/Search/Search.jsx
@@ -1,11 +1,13 @@
-import style from 'Search.module.css'
+import style from './Search.module.css'
+import SearchIcon from '../../../../Assets/Icons/IconsComponents/SearchIcon'
 
 const Search = () => {
+  
   return(
-    <>
+    <div className={style.Container}>
       <input type="text" placeholder='Buscar' className={style.SearchInput}/>
-      <button className={style.SearchButton}>Buscar</button>
-    </>
+      <button className={style.SearchButton}> <SearchIcon Color={'white'}/> </button>
+    </div>
   )
 }
 
